68c4cd10229969c18c0816c9f061ec1b7d04cbc2
lbh0104 <dlqudgnswk@naver.com>
2019-03-12 21:35:50 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import {withStyles} from '@material-ui/core/styles';
-
 
 const styles = theme => ({
 
@@ -20,21 +20,25 @@ const styles = theme => ({
     },
     table: {
         minWidth: 1080
+    },
+    progress: {
+        margin: theme.spacing.unit * 2
     }
-
 })
-
 
 class App extends Component {
 
     state = {
-        customers: ""
+        customers: "",
+        completed: 0
     }
 
-    componentDidMount(){
-        this.callApi()
-        .then(res => this.setState({customers: res}))
-        .catch(err => console.log(err));
+    componentDidMount() {
+        this.timer = setInterval(this.progress, 20);
+        this
+            .callApi()
+            .then(res => this.setState({customers: res}))
+            .catch(err => console.log(err));
     }
 
     callApi = async() => {
@@ -43,8 +47,17 @@ class App extends Component {
         return body;
     }
 
+    progress = () => {
+        const {completed} = this.state;
+        this.setState({
+            completed: completed >= 100
+                ? 0
+                : completed + 1
+        })
+    }
+
     render() {
-      const {classes}  = this.props;
+        const {classes} = this.props;
         return (
             <Paper className={classes.root}>
                 <Table className={classes.table}>
@@ -59,17 +72,29 @@ class App extends Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.customers ? this.state.customers.map(c => {
-                            return (<Customer
-                                key={c.id}
-                                id={c.id}
-                                name={c.name}
-                                image={c.image}
-                                gender={c.gender}
-                                job={c.job}
-                                birthday={c.birthday}/>);
-                        }) : "로딩"
-                    }
+                        {this.state.customers
+                            ? this
+                                .state
+                                .customers
+                                .map(c => {
+                                    return (<Customer
+                                        key={c.id}
+                                        id={c.id}
+                                        name={c.name}
+                                        image={c.image}
+                                        gender={c.gender}
+                                        job={c.job}
+                                        birthday={c.birthday}/>);
+                                })
+                            : <TableRow>
+                                <TableCell colSpan="6" align="center">
+                                    <CircularProgress
+                                        className={classes.progress}
+                                        variant="determinate"
+                                        value={this.state.completed}/>
+                                </TableCell>
+                            </TableRow>
+}
                     </TableBody>
                 </Table>
             </Paper>
